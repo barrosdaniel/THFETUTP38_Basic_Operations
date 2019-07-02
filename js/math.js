50d278a3ed18bf451e029e95900eccee3098b389
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-07-02 22:26:55 +0800

    feature: Add input validation for 1) second number is zero, and 2) numbers and NaN

--- js/math.js
+++ js/math.js
@@ -1,15 +1,22 @@
+var number1;
+var number2;
+var message;
+
 alert('Let\'s do some Math!');
 
-var number1 = prompt('What is the first number?');
-number1 = parseFloat(number1);
+number1 = parseFloat(prompt('What is the first number?'));
+number2 = parseFloat(prompt('What is the second number?'));
 
-var number2 = prompt('What is the second number?');
-number2 = parseFloat(number2);
+if (number2 === 0) {
+  alert("The second number is 0. You can't divide by zero. Reload and try again.");
+} else if (isNaN(number1) || isNaN(number2)) {
+  alert("At least one of the values you typed is not a number. Reload and try again.");
+} else {
+  message = '<h1>Math with the numbers ' + number1 + ' and ' + number2 + '</h1>';
+  message += '<p>' + number1 + ' + ' + number2 + ' = ' + (number1 + number2) + '</p>';
+  message += '<p>' + number1 + ' * ' + number2 + ' = ' + (number1 * number2) + '</p>';
+  message += '<p>' + number1 + ' / ' + number2 + ' = ' + (number1 / number2) + '</p>';
+  message += '<p>' + number1 + ' - ' + number2 + ' = ' + (number1 - number2) + '</p>';
 
-var message = '<h1>Math with the numbers ' + number1 + ' and ' + number2 + '</h1>';
-message += '<p>' + number1 + ' + ' + number2 + ' = ' + (number1 + number2) + '</p>';
-message += '<p>' + number1 + ' * ' + number2 + ' = ' + (number1 * number2) + '</p>';
-message += '<p>' + number1 + ' / ' + number2 + ' = ' + (number1 / number2) + '</p>';
-message += '<p>' + number1 + ' - ' + number2 + ' = ' + (number1 - number2) + '</p>';
-
-document.write(message);
+  document.write(message);
+}
